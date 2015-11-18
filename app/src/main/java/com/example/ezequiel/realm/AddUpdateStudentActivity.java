a8feed66e48fc3c4b053e5789f841079b0e1fba0
Ezequiel <zeki-san@hotmail.com>
2015-11-17 22:12:31 -0200

    Aula 3

--- app/src/main/java/com/example/ezequiel/realm/AddUpdateStudentActivity.java
+++ app/src/main/java/com/example/ezequiel/realm/AddUpdateStudentActivity.java
@@ -25,7 +25,7 @@ import java.util.List;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
-public class AddUpdateStudentActivity extends AppCompatActivity{
+public class AddUpdateStudentActivity extends AppCompatActivity {
 
     private Realm realm;
     private RealmResults<Student> students;
@@ -46,16 +46,30 @@ public class AddUpdateStudentActivity extends AppCompatActivity{
 
 
         realm = Realm.getDefaultInstance();
-        students = realm.where( Student.class ).findAll();
+        students = realm.where(Student.class).findAll();
 
-        if( getIntent() != null && getIntent().getLongExtra( Student.ID, 0 ) > 0 ){
-            student.setId( getIntent().getLongExtra( Student.ID, 0 ) );
+        if (getIntent() != null && getIntent().getLongExtra(Student.ID, 0) > 0) {
+            student.setId(getIntent().getLongExtra(Student.ID, 0));
 
             student = students.where().equalTo("id", student.getId()).findAll().get(0);
-            etName.setText( student.getName() );
+            etName.setText(student.getName());
             tvTitle.setText("Atualizar disciplina");
-            btAddUpdate.setText( "Update" );
+            btAddUpdate.setText("Update");
         }
+
+        disciplines = realm.where(Discipline.class).findAll();
+
+        Spinner spDiscipline = (Spinner) findViewById(R.id.sp_discipline);
+        spDiscipline.setAdapter(new DisciplineSpinnerAdapter(this, disciplines));
+
+        //first block
+        View btRemove = findViewById(R.id.bt_remove);
+        btRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callRemoveGrade(v );
+            }
+        });
     }
 
 
@@ -67,16 +81,16 @@ public class AddUpdateStudentActivity extends AppCompatActivity{
 
 
     // LISTENERS
-    public void callAddUpdateStudent( View view ){
+    public void callAddUpdateStudent(View view) {
         String label = "atualizado";
-        if( student.getId() == 0 ){
+        if (student.getId() == 0) {
             students.sort("id", RealmResults.SORT_ORDER_DESCENDING);
             long id = students.size() == 0 ? 1 : students.get(0).getId() + 1;
             student.setId(id);
             label = "adicionado";
         }
 
-        try{
+        try {
             realm.beginTransaction();
             student.setName(etName.getText().toString());
             realm.copyToRealmOrUpdate(student);
@@ -86,22 +100,21 @@ public class AddUpdateStudentActivity extends AppCompatActivity{
 
             realm.beginTransaction();
             student.getGrades().clear();
-            student.getGrades().addAll( getGradesFromView( view, disciplines ) );
+            student.getGrades().addAll(getGradesFromView(view, disciplines));
             realm.commitTransaction();
 
             Toast.makeText(AddUpdateStudentActivity.this, "Estudante " + label, Toast.LENGTH_SHORT).show();
             finish();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             Toast.makeText(AddUpdateStudentActivity.this, "Falhou!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void callAddGrade( View view ){
+    public void callAddGrade(View view) {
         LinearLayout llParent = (LinearLayout) view.getParent();
 
-        if( llParent.getChildCount() - 1 == disciplines.size() ){
+        if (llParent.getChildCount() - 1 == disciplines.size()) {
             Toast.makeText(this, "Máximo de disciplinas atingido", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -109,27 +122,27 @@ public class AddUpdateStudentActivity extends AppCompatActivity{
         createGradeForView(view, disciplines, null);
     }
 
-    private void callRemoveGrade( View view ){
+    private void callRemoveGrade(View view) {
         LinearLayout llParent = (LinearLayout) view.getParent().getParent();
 
-        if( llParent.getChildCount() > 2 ){
+        if (llParent.getChildCount() > 2) {
             llParent.removeView((LinearLayout) view.getParent());
         }
     }
 
     // UTIL
-    private void createGradeForView( View view, RealmResults<Discipline> disciplines, Grade grade ){
+    private void createGradeForView(View view, RealmResults<Discipline> disciplines, Grade grade) {
         LayoutInflater inflater = this.getLayoutInflater();
         LinearLayout llChild = (LinearLayout) inflater.inflate(R.layout.box_discipline_grade, null);
 
         Spinner spDiscipline = (Spinner) llChild.findViewById(R.id.sp_discipline);
-        spDiscipline.setAdapter( new DisciplineSpinnerAdapter( this, disciplines ));
-        if( grade != null ){
+        spDiscipline.setAdapter(new DisciplineSpinnerAdapter(this, disciplines));
+        if (grade != null) {
             spDiscipline.setSelection(getDisciplinePosition(disciplines, grade.getDiscipline()));
         }
 
         EditText etGrade = (EditText) llChild.findViewById(R.id.et_grade);
-        if( grade != null ){
+        if (grade != null) {
             etGrade.setText(String.valueOf(grade.getGrade()));
         }
 
@@ -137,67 +150,66 @@ public class AddUpdateStudentActivity extends AppCompatActivity{
         btRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                callRemoveGrade( v );
+                callRemoveGrade(v);
             }
         });
 
         float scale = getResources().getDisplayMetrics().density;
-        int margin = (int)( 5 * scale + 0.5f );
+        int margin = (int) (5 * scale + 0.5f);
 
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins( margin, margin, margin, margin );
-        llChild.setLayoutParams( layoutParams );
+        layoutParams.setMargins(margin, margin, margin, margin);
+        llChild.setLayoutParams(layoutParams);
 
         LinearLayout llParent = (LinearLayout) view.getParent();
-        llParent.addView( llChild, llParent.getChildCount() - 1 );
+        llParent.addView(llChild, llParent.getChildCount() - 1);
     }
 
-    private int getDisciplinePosition( RealmResults<Discipline> disciplines, Discipline discipline ){
-        for( int i = 0; i < disciplines.size(); i++ ){
-            if( disciplines.get(i).getId() == discipline.getId() ){
-                return( i );
+    private int getDisciplinePosition(RealmResults<Discipline> disciplines, Discipline discipline) {
+        for (int i = 0; i < disciplines.size(); i++) {
+            if (disciplines.get(i).getId() == discipline.getId()) {
+                return (i);
             }
         }
-        return(0);
+        return (0);
     }
 
-    private List<Grade> getGradesFromView( View view, RealmResults<Discipline> disciplines ){
+    private List<Grade> getGradesFromView(View view, RealmResults<Discipline> disciplines) {
         List<Grade> list = new LinkedList<>();
         RelativeLayout rlParent = (RelativeLayout) view.getParent();
 
-        for( int i = 0; i < rlParent.getChildCount(); i++ ){
+        for (int i = 0; i < rlParent.getChildCount(); i++) {
 
-            if( rlParent.getChildAt( i ) instanceof ScrollView){
-                ScrollView scrollView = (ScrollView) rlParent.getChildAt( i );
-                LinearLayout llChild = (LinearLayout) scrollView.getChildAt( 0 );
+            if (rlParent.getChildAt(i) instanceof ScrollView) {
+                ScrollView scrollView = (ScrollView) rlParent.getChildAt(i);
+                LinearLayout llChild = (LinearLayout) scrollView.getChildAt(0);
 
-                for( int j = 0; j < llChild.getChildCount(); j++ ){
-                    if( llChild.getChildAt( j ) instanceof LinearLayout ){
+                for (int j = 0; j < llChild.getChildCount(); j++) {
+                    if (llChild.getChildAt(j) instanceof LinearLayout) {
 
-                        Spinner spDiscipline = (Spinner) llChild.getChildAt( j ).findViewById(R.id.sp_discipline);
-                        EditText etGrade = (EditText) llChild.getChildAt( j ).findViewById(R.id.et_grade);
+                        Spinner spDiscipline = (Spinner) llChild.getChildAt(j).findViewById(R.id.sp_discipline);
+                        EditText etGrade = (EditText) llChild.getChildAt(j).findViewById(R.id.et_grade);
 
                         Grade g = new Grade();
-                        if( realm.where(Grade.class).findAll().size() > 0 ){
-                            g.setId( realm.where(Grade.class).findAll().where().findAllSorted("id", RealmResults.SORT_ORDER_DESCENDING).get(0).getId() + 1 + j );
-                        }
-                        else{
+                        if (realm.where(Grade.class).findAll().size() > 0) {
+                            g.setId(realm.where(Grade.class).findAll().where().findAllSorted("id", RealmResults.SORT_ORDER_DESCENDING).get(0).getId() + 1 + j);
+                        } else {
                             g.setId(1);
                         }
 
                         Discipline d = new Discipline();
-                        d.setId( disciplines.get( spDiscipline.getSelectedItemPosition() ).getId() );
+                        d.setId(disciplines.get(spDiscipline.getSelectedItemPosition()).getId());
                         d.setName(disciplines.get(spDiscipline.getSelectedItemPosition()).getName());
                         g.setDiscipline(d);
 
                         g.setGrade(Double.parseDouble(etGrade.getText().toString()));
 
-                        list.add( g );
+                        list.add(g);
                     }
                 }
             }
         }
-        return( list );
+        return (list);
     }
 
 }
